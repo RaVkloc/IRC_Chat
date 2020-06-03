b667eed02b290b43d6dafc655d85cda6f31c8971
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2020-06-03 18:09:01 +0200

    Emiters and listeners example fixed.

--- xclient_gui/dev_help/QtSignalsAndSlots.py
+++ xclient_gui/dev_help/QtSignalsAndSlots.py
@@ -5,23 +5,33 @@ class Dowolna(QObject):
     trigger = pyqtSignal()
 
 
+ob = Dowolna()
+
 class Foo(QObject):
-    # Define a new signal called 'trigger' that has no arguments.
-    # trigger = Dowolna
-
     def connect_and_emit_trigger(self):
-        # Connect the trigger signal to a slot.
-        ob = Dowolna()
+        # ob = Dowolna()
+        print('connect')
+        print(id(ob.trigger))
         ob.trigger.connect(self.handle_trigger)
-
-        # Emit the signal.
-        ob.trigger.emit()
+        print(id(ob.trigger))
 
     def handle_trigger(self):
-        # Show that the slot has been called.
-
         print("trigger signal received")
 
 
+class Foo2(QObject):
+    def con(self):
+        # ob = Dowolna()
+        print('emit')
+        print(id(ob.trigger))
+        ob.trigger.emit()
+        print(id(ob.trigger))
+
+
+foo2 = Foo2()
+foo2.con()
+
 foo = Foo()
 foo.connect_and_emit_trigger()
+
+
