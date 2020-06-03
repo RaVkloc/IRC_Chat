09db2fff2a4246362d71c6b5f20f0d0eaf8c032e
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2020-06-03 23:35:34 +0200

    RegisterForm IMPLEMENTED.

--- xclient_gui/desktop/views/register.py
+++ xclient_gui/desktop/views/register.py
@@ -1,38 +1,29 @@
-import sys
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QGridLayout, QMessageBox)
-from xclient_gui.desktop.controllers.clientMediator import Mediator
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QGridLayout)
+
 from xclient_gui.desktop.views.components.formInput import FormInput
+from xclient_gui.desktop.views.base.BaseWidget import BaseWidget
+from xcomm.xcomm_moduledefs import MESSAGE_ACTION_REGISTER_LOGIN, MESSAGE_ACTION_REGISTER_PASSWORD
 
 
-class RegisterForm(QWidget):
+class RegisterForm(QWidget, BaseWidget):
     open_next_screen = QtCore.pyqtSignal()
-    open_registration = QtCore.pyqtSignal()
 
     def __init__(self, client):
         super().__init__()
         self.client = client
-        self.slot = None
-        
         self.lineEdit_username = None
         self.lineEdit_password = None
         self.lineEdit_confirm_password = None
 
-        self.connect_to_slot()
         self.create_gui()
-
-    def connect_to_slot(self):
-        self.slot = Mediator()
-        self.slot.signal.connect(self.handle_receive)
 
     def handle_receive(self, response):
         status = response.message.body['Status']
         if status == 'OK':
             self.open_next_screen.emit()
         else:
-            msg = QMessageBox()
-            msg.setText(status)
-            msg.exec_()
+            self.show_error_box(status)
 
     def create_gui(self):
         self.set_widget_default_values()
@@ -40,13 +31,13 @@ class RegisterForm(QWidget):
         layout = QGridLayout()
         self.create_username_input(layout)
         self.create_password_input(layout)
-        self.create_login_button(layout)
-        self.create_register_encouragement(layout)
+        self.create_confirm_password_input(layout)
+        self.create_register_button(layout)
 
         self.setLayout(layout)
 
     def set_widget_default_values(self):
-        self.setWindowTitle('Login Form')
+        self.setWindowTitle('Register')
         self.resize(500, 120)
 
     def create_username_input(self, layout):
@@ -62,27 +53,29 @@ class RegisterForm(QWidget):
         layout.addWidget(label_password, 1, 0)
         layout.addWidget(self.lineEdit_password, 1, 1)
 
-    def create_login_button(self, layout):
-        button_login = QPushButton('Login')
-        button_login.clicked.connect(self.login)
-        button_login.setStyleSheet("padding-top: 5px; padding-bottom: 5px;")
-        layout.addWidget(button_login, 2, 0, 1, 2)
-        layout.setRowMinimumHeight(2, 75)
+    def create_confirm_password_input(self, layout):
+        label_confirm_password, self.lineEdit_confirm_password = FormInput('Confirm Password:',
+                                                                           "Confirm your password").get_input()
+        self.lineEdit_confirm_password.setEchoMode(QLineEdit.Password)
 
-    def create_register_encouragement(self, layout):
-        register = QPushButton("Don't have account? REGISTER")
-        register.clicked.connect(self.asd)
-        register.setStyleSheet("font-size: 9px;")
-        layout.addWidget(register, 3, 2, 1, 2)
+        layout.addWidget(label_confirm_password, 2, 0)
+        layout.addWidget(self.lineEdit_confirm_password, 2, 1)
 
-    def asd(self):
-        self.open_registration.emit()
+    def create_register_button(self, layout):
+        button_register = QPushButton('Register')
+        button_register.clicked.connect(self.register)
+        button_register.setStyleSheet("padding-top: 5px; padding-bottom: 5px;")
+        layout.addWidget(button_register, 3, 0, 1, 2)
 
-    def login(self):
+    def register(self):
         login = self.lineEdit_username.text()
         password = self.lineEdit_password.text()
-        body = {
-            "login": login,
-            "password": password
-        }
-        self.client.login(body=body)
+        confirm_password = self.lineEdit_confirm_password.text()
+        if password == confirm_password:
+            body = {
+                MESSAGE_ACTION_REGISTER_LOGIN: login,
+                MESSAGE_ACTION_REGISTER_PASSWORD: password
+            }
+            self.client.register(body=body)
+        else:
+            self.show_error_box("Passwords are not equal.")
