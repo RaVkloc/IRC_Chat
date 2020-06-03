fd6cca1c280af468034ceed0c2f28c73086dc21a
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2020-06-03 22:55:17 +0200

    Working on Login Screen, Register base created.

--- xclient_gui/desktop/views/login.py
+++ xclient_gui/desktop/views/login.py
@@ -1,17 +1,31 @@
 import sys
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QMessageBox)
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit, QGridLayout, QMessageBox)
+from xclient_gui.desktop.controllers.clientMediator import Mediator
+from xclient_gui.desktop.views.components.formInput import FormInput
+from xclient_gui.desktop.views.base.BaseWidget import BaseWidget
 
 
-class LoginForm(QWidget):
-    switch_window = QtCore.pyqtSignal()
+class LoginForm(QWidget, BaseWidget):
+    open_next_screen = QtCore.pyqtSignal()
+    open_registration = QtCore.pyqtSignal()
 
-    def __init__(self):
+    def __init__(self, client):
         super().__init__()
+        self.client = client
         self.lineEdit_username = None
         self.lineEdit_password = None
 
         self.create_gui()
+
+    def handle_receive(self, response):
+        status = response.message.body['Status']
+        if status == 'OK':
+            self.open_next_screen.emit()
+        else:
+            msg = QMessageBox()
+            msg.setText(status)
+            msg.exec_()
 
     def create_gui(self):
         self.set_widget_default_values()
@@ -20,6 +34,7 @@ class LoginForm(QWidget):
         self.create_username_input(layout)
         self.create_password_input(layout)
         self.create_login_button(layout)
+        self.create_register_encouragement(layout)
 
         self.setLayout(layout)
 
@@ -28,38 +43,40 @@ class LoginForm(QWidget):
         self.resize(500, 120)
 
     def create_username_input(self, layout):
-        label_name = QLabel('<font size="4"> Username: </font>')
-        self.lineEdit_username = QLineEdit()
-        self.lineEdit_username.setPlaceholderText('Please enter your username')
+        label_name, self.lineEdit_username = FormInput('Username:', "Enter username").get_input()
+
         layout.addWidget(label_name, 0, 0)
         layout.addWidget(self.lineEdit_username, 0, 1)
 
     def create_password_input(self, layout):
-        label_password = QLabel('<font size="4"> Password: </font>')
-        self.lineEdit_password = QLineEdit()
-        self.lineEdit_password.setPlaceholderText('Please enter your password')
+        label_password, self.lineEdit_password = FormInput('Password:', "Enter password").get_input()
         self.lineEdit_password.setEchoMode(QLineEdit.Password)
+
         layout.addWidget(label_password, 1, 0)
         layout.addWidget(self.lineEdit_password, 1, 1)
 
     def create_login_button(self, layout):
         button_login = QPushButton('Login')
         button_login.clicked.connect(self.login)
+        button_login.setStyleSheet("padding-top: 5px; padding-bottom: 5px;")
         layout.addWidget(button_login, 2, 0, 1, 2)
         layout.setRowMinimumHeight(2, 75)
 
+    def create_register_encouragement(self, layout):
+        register = QPushButton("Don't have account? REGISTER")
+        register.clicked.connect(self.asd)
+        register.setStyleSheet("font-size: 9px;")
+        layout.addWidget(register, 3, 2, 1, 2)
+
+    def asd(self):
+        print("dsfsdd")
+        self.open_registration.emit()
+
     def login(self):
-
-        if self.lineEdit_username.text() == 'a' and self.lineEdit_password.text() == 'a':
-            # dashboard = Dashboard()
-            # dashboard.show()
-            # self.hide()
-            # app.quit()
-            self.switch_window.emit()
-
-        else:
-            msg = QMessageBox()
-            msg.setText('Incorrect Password')
-            msg.exec_()
-
-
+        login = self.lineEdit_username.text()
+        password = self.lineEdit_password.text()
+        body = {
+            "login": login,
+            "password": password
+        }
+        self.client.login(body=body)
