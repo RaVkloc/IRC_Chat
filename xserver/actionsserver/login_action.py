99f5f6df518b978c15c7bfc4b3f0e8e5561f01eb
klocrafi <rafal.kloc@live.umcs.edu.pl>
2020-06-04 20:21:22 +0200

    ADDED additional logs for server login action.

--- xserver/actionsserver/login_action.py
+++ xserver/actionsserver/login_action.py
@@ -1,9 +1,12 @@
 import hashlib
+import logging
 from uuid import uuid4
 
 from xserver.actionsserver.action_base import ActionBase
 from xcomm.xcomm_moduledefs import *
 from xserver.actionsserver.exceptions import InvalidLoginData, InvalidTokenSave
+
+logger = logging.getLogger("LoginAction")
 
 
 class LoginAction(ActionBase):
@@ -15,6 +18,7 @@ class LoginAction(ActionBase):
         return MESSAGE_ACTION_LOGIN_CODE
 
     def execute(self):
+        logger.debug("Starting executing LOGIN action.")
         login = self.msg.get_body_param(MESSAGE_ACTION_LOGIN_LOGIN)
         passwd = self.msg.get_body_param(MESSAGE_ACTION_LOGIN_PASSWORD)
 
@@ -26,6 +30,7 @@ class LoginAction(ActionBase):
                 result = self._get_user_data_from_db(login, cursor)
             except InvalidLoginData as e:
                 self.set_error_with_status(e.message)
+                logger.error(e.message)
                 return
 
             # empty means no such a user in DB
@@ -33,17 +38,21 @@ class LoginAction(ActionBase):
             if not result or result[1] != passwd:
                 # send the same info in two cases so as no to say if such a user exists
                 self.set_error_with_status("Invalid username or password.")
+                logger.error("No user found or password does not match its hash.")
                 return
 
             token = str(uuid4())
+            logger.debug("Generated token: " + token)
             try:
                 self._add_user_token_to_db(result[0], token, cursor)
             except InvalidTokenSave as e:
                 self.set_error_with_status(e.message)
+                logger.error(e.message)
                 return
 
         self.result.add_body_param(MESSAGE_ACTION_LOGIN_TOKEN, token)
         self.set_status_ok()
+        logger.debug("Action LOGIN executed SUCCESSFULLY.")
 
     def _get_user_password_hash(self, password):
         hash_alg = hashlib.sha3_256()
@@ -51,14 +60,20 @@ class LoginAction(ActionBase):
         return hash_alg.hexdigest()
 
     def _get_user_data_from_db(self, login, cursor):
-        query = "SELECT `username`, `password` FROM `users_user` WHERE `username` = %s"
+        query = "SELECT username, password FROM users_user WHERE username = '{}'"
+
+        logger.debug("Executing query: " + query + "\n\twith params: " + str((login,)))
 
         # return None if user not found
-        cursor.execute(query, (login,))
-        return cursor.fetchone()
+        cursor.execute(query.format(login))
+        result = cursor.fetchone()
+        logger.debug("Query result: " + str(result))
+        return result
 
     def _add_user_token_to_db(self, username, user_token, cursor):
-        sql_query_add_token = "UPDATE users_user SET token = '{}' WHERE username = '{}'"
+        query = "UPDATE users_user SET token = '{}' WHERE username = '{}'"
 
-        cursor.execute(sql_query_add_token.format(user_token, username))
+        logger.debug("Executing query: " + query + "\n\twith params: " + str((username, user_token)))
+
+        cursor.execute(query.format(user_token, username))
         self.db_connect.connection.commit()
