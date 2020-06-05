da16c1e1ee7d3a8027fa931ef27e06a875cfb5ca
klocrafi <rafal.kloc@live.umcs.edu.pl>
2020-06-05 11:01:32 +0200

    ADDED additional logs in server 'logout' action.

--- xserver/actionsserver/logout_action.py
+++ xserver/actionsserver/logout_action.py
@@ -5,6 +5,8 @@ from xserver.actionsserver.decorators import login_required
 
 from xcomm.xcomm_moduledefs import MESSAGE_ACTION_LOGOUT_CODE
 
+logger = logging.getLogger("LogoutAction")
+
 
 class LogoutAction(ActionBase):
     def get_action_number(self):
@@ -16,12 +18,13 @@ class LogoutAction(ActionBase):
             self._logout_user_in_db(self.user, cursor)
 
             self.set_status_ok()
+            logger.debug("(userID={}Executing action LOGOUT finished SUCCESSFULLY.".format(self.user))
 
     def _logout_user_in_db(self, user_id, cursor):
-        logger = logging.getLogger("LogoutAction")
         query = "UPDATE users_user SET token = null, room_id_id = null WHERE id = {}"
 
-        logger.debug("Executing query: " + query + "\n\twith params: " + str(self.user))
+        logger.debug("(userID={})Executing query: ".format(self.user) + query + "\n\twith params: " + str(self.user))
 
         cursor.execute(query.format(user_id))
         self.db_connect.connection.commit()
+        logger.debug("(userID={})Query executed successfully.".format(self.user))
