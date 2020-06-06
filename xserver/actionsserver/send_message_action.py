3f03ecf867d438797b346f663b0bfc7be773d78e
klocrafi <rafal.kloc@live.umcs.edu.pl>
2020-06-06 10:23:02 +0200

    ADDED additional logs for server 'send message' action.

--- xserver/actionsserver/send_message_action.py
+++ xserver/actionsserver/send_message_action.py
@@ -1,7 +1,12 @@
-from xcomm.xcomm_moduledefs import MESSAGE_ACTION_SENDMESSAGE_CODE
+import logging
+
 from xserver.actionsserver.action_base import ActionBase
 from xserver.actionsserver.decorators import login_required
 from xserver.actionsserver.exceptions import NoActiveRoomException
+
+from xcomm.xcomm_moduledefs import MESSAGE_ACTION_SENDMESSAGE_CODE
+
+logger = logging.getLogger("SendMessageAction")
 
 
 class SendMessageAction(ActionBase):
@@ -10,6 +15,7 @@ class SendMessageAction(ActionBase):
 
     @login_required
     def execute(self, *args, **kwargs):
+        logger.debug("(userID={})Executing SEND_MESSAGE action started.".format(self.user))
         with self.db_connect as cursor:
             try:
                 room = self._get_room_by_user(self.user, cursor)
@@ -20,24 +26,40 @@ class SendMessageAction(ActionBase):
                 self.set_error_with_status(e.message)
                 return
 
+            self.set_status_ok()
+            logger.debug("(userID={})Executing SEND_MESSAGE action SUCCESSFULLY finished.".format(self.user))
+
     def _get_room_by_user(self, user_id, cursor):
         query = 'SELECT room_id_id FROM users_user WHERE id ={}'
+
+        logger.debug("(userID={})Executing query: ".format(self.user) + query + "\n\twith params: " + str((user_id,)))
+
         cursor.execute(query.format(user_id))
         result = cursor.fetchone()
+        logger.debug("(userID={})Query result: ".format(self.user) + str(result))
+
         if not result:
             raise NoActiveRoomException()
         return result[0]
 
     def _get_list_users_in_room(self, room_id, cursor):
         query = 'SELECT id FROM users_user WHERE room_id_id={}'
+
+        logger.debug("(userID={})Executing query: ".format(self.user) + query + "\n\twith params: " + str((room_id,)))
+
         cursor.execute(query.format(room_id))
         result = cursor.fetchall()
+        logger.debug("(userID={})Query result: ".format(self.user) + str(result))
+
         return result
 
     def get_client_list(self, users):
         from xserver.coreserver.server_core import CoreServer
         client_list = CoreServer.__instance.clients
-        return list(filter(lambda client: client.user in users, client_list))
+        clients = list(filter(lambda client: client.user in users, client_list))
+        logger.debug("(userID={})Users selected to send message to: ".format(self.user) + str(clients))
+
+        return clients
 
     def send_message(self, client_list):
         message = self.msg.convert_message_to_bytes()
