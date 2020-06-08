d2e1be9db930402e105ed396076004b9bb6ebac5
klocrafi <rafal.kloc@live.umcs.edu.pl>
2020-06-08 11:41:52 +0200

    CHANGED behavior of directing message (and its content) to send to others users in room.

--- xserver/actionsserver/send_message_action.py
+++ xserver/actionsserver/send_message_action.py
@@ -1,10 +1,12 @@
 import logging
+import datetime
 
 from xserver.actionsserver.action_base import ActionBase
 from xserver.actionsserver.decorators import login_required
 from xserver.actionsserver.exceptions import NoActiveRoomException
 
-from xcomm.xcomm_moduledefs import MESSAGE_ACTION_SENDMESSAGE_CODE
+from xcomm.xcomm_moduledefs import MESSAGE_ACTION, MESSAGE_ACTION_SENDMESSAGE_CODE, MESSAGE_ACTION_RECVMESSAGE_CODE, \
+    MESSAGE_ACTION_RECVMESSAGE_TIMESTAMP, MESSAGE_ACTION_RECVMESSAGE_USER
 
 logger = logging.getLogger("SendMessageAction")
 
@@ -20,8 +22,9 @@ class SendMessageAction(ActionBase):
             try:
                 room = self._get_room_by_user(self.user, cursor)
                 users = self._get_list_users_in_room(room, cursor)
+                current_username = self._get_username_by_id(cursor)
                 clients = self.get_client_list(users, server)
-                self.send_message(clients)
+                self.send_message(clients, current_username)
             except NoActiveRoomException as e:
                 self.set_error_with_status(e.message)
                 return
@@ -52,10 +55,34 @@ class SendMessageAction(ActionBase):
         return list(map(lambda user: user[0], result))
 
     def get_client_list(self, users, server):
-        logger.debug("(userID={})Users selected to send message to: ".format(self.user))
-        return filter(lambda client: client.user in users, server.clients)
+        client_list = filter(lambda client: client.user in users, server.clients)
+        # logger.debug("(userID={})Users selected to send message to: ".format(self.user) + str(client_list))
 
-    def send_message(self, client_list):
-        message = self.msg.convert_message_to_bytes()
+        return client_list
+
+    def send_message(self, client_list, current_username):
+        from xcomm.message import Message
+        message = Message(body=self.msg.body)
+        message.add_header_param(MESSAGE_ACTION, MESSAGE_ACTION_RECVMESSAGE_CODE)
+
+        # Adding info about user and current date&time
+        message.add_body_param(MESSAGE_ACTION_RECVMESSAGE_USER, current_username)
+        time_now = datetime.datetime.now()
+        message.add_body_param(MESSAGE_ACTION_RECVMESSAGE_TIMESTAMP, time_now.timestamp())
+
+        logger.debug("Directing message to all users in room: " + message.get_complete_message().replace('\0', '\n'))
+
+        message = message.convert_message_to_bytes()
+
         for client in client_list:
             client.client_socket.sendall(message)
+
+    def _get_username_by_id(self, cursor):
+        query = "SELECT username FROM users_user WHERE id = {}"
+
+        logger.debug("(userID={})Executing query: ".format(self.user) + query + "\n\twith params: " + str(self.user))
+        cursor.execute(query.format(self.user))
+        result = cursor.fetchone()
+
+        logger.debug("(userID={})Query result: ".format(self.user) + str(result))
+        return result[0]
