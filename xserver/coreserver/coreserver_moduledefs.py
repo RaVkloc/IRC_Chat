6770d33483e1def5f84c1453c435f1c3947801fb
klocrafi <rafal.kloc@live.umcs.edu.pl>
2020-06-17 18:49:30 +0200

    Generating unique new filename for server logs every time server starts.

--- xserver/coreserver/coreserver_moduledefs.py
+++ xserver/coreserver/coreserver_moduledefs.py
@@ -1,16 +1,23 @@
-import os
+def find_unique_filename():
+    import os
+    num = 1
+    while os.path.isfile(SERVER_LOG_FILENAME.format(num)):
+        num += 1
+    return SERVER_LOG_FILENAME.format(num)
+
 
 SERVER_ADDRESS = "localhost"
 SERVER_PORT = 6111
 SERVER_MAX_CONNECTION = 5
 
-SERVER_LOG_FILENAME = "../../server.log"
+SERVER_LOG_FILENAME = "../../server.log{}"
 SERVER_LOG_FORMAT = "%(asctime)s %(levelname)s [%(name)s]: %(message)s"
 SERVER_LOG_DATEFORMAT = "%H:%M:%S %m/%d/%Y"
-SERVER_LOG_CONFIG = {"filename": "../../server.log",
+
+SERVER_LOG_CONFIG = {"filename": find_unique_filename(),
                      "filemode": "w+",
-                     "format": "%(asctime)s %(levelname)s [%(name)s]: %(message)s",
-                     "datefmt": "%H:%M:%S %d/%m/%Y",
+                     "format": SERVER_LOG_FORMAT,
+                     "datefmt": SERVER_LOG_DATEFORMAT,
                      "level": "DEBUG"
                      }
 
