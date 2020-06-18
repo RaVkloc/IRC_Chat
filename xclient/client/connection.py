1789313785d905303615776c4d8fc02cedbdddf7
klocrafi <rafal.kloc@live.umcs.edu.pl>
2020-06-18 22:37:32 +0200

    REMOVED unnecessary comments and print.

--- xclient/client/connection.py
+++ xclient/client/connection.py
@@ -23,8 +23,7 @@ class Connection:
             raise ConnectionRefusedError(e)
 
     def __enter__(self):
-        # try:
-        # self.socket.connect((self.ip, self.port))
+
         if TLS:
             self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
             self.context.verify_mode = ssl.CERT_REQUIRED
@@ -33,15 +32,11 @@ class Connection:
             self.secure_socket = self.context.wrap_socket(self.socket, server_side=False, server_hostname=self.ip)
         self.connected = True
         return self
-        # except socket.error:
-        #     self.connected = False
-        #     # raise ConnectionRefusedError("sdfssd")
 
     def get_socket(self):
         return self.secure_socket if TLS else self.socket
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print(exc_type, exc_val, exc_tb)
         if self.client.token:
             self.client.logout()
 
