8cef5f10c94550d663b32f99e082ccedabf08e58
Marek Zięba <m.zieba1994@gmail.com>
2020-06-19 23:29:44 +0200

    kwargs fix

--- xclient/client/decorators.py
+++ xclient/client/decorators.py
@@ -5,7 +5,7 @@ from xclient.client.utils import Sender
 def request_action():
     def inner(f):
         def func(*args, **kwargs):
-            Sender.send_message(f, *args, kwargs)
+            Sender.send_message(f, *args, **kwargs)
 
         return func
 
